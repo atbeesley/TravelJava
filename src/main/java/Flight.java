f4c24713a3ab860910fa7c07c4971fda6ec8f27e
user <you@exampalex.com>
2019-11-22 16:35:45 +0000

    adds Flight and FlightTest

---
+++ src/main/java/Flight.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Flight {
+
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destination;
+    private String departureAirport;
+    private String departureTime;
+
+}
